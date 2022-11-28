e04b030afeba1448ae08b04469f0dbb10521496b
Амаль <50665935+WTTeneger@users.noreply.github.com>
2022-11-28 17:56:02 +0700

    add

---
+++ pl/lite/widget/script.js
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		
+	
+	
+	
+		<title></title>
+			
+<script>
+	
+	window.accountUserId = -1;
+	window.requestIp = "139.180.211.179";
+	window.isSublogined = false;
+	window.accountId = 558766;
+	window.gcSessionId = 0;
+	window.controllerId = "error";
+	window.actionId = "error";
+    window.nowTime = new Date(2022, 10, 14, 18, 22, 55);
+    window.dateOffset = 3;
+	window.userInfo = {"isAdmin":false,"isInAccount":false,"isTeacher":false};
+	window.websocketHost = "s163.getcourse.ru";
+		window.websocketHttpPort = 32081;
+	window.websocketHttpsPort = 32443;
+	window.currencyLabels = {"RUB":"RUB","USD":"USD","EUR":"EUR","GBP":"GBP","BYR":"BYR","BYN":"BYN","KZT":"KZT","UAH":"UAH","AUD":"AUD","DKK":"DKK","CHF":"CHF","SEK":"SEK","ZAR":"ZAR","AMD":"AMD","RON":"RON","BRL":"BRL","ILS":"ILS","MYR":"MYR","SGD":"SGD","KGS":"KGS","CAD":"CAD","MXN":"MXN","JPY":"JPY","UZS":"UZS","PLN":"PLN","AZN":"AZN","AED":"AED","TRY":"TRY","INR":"INR","RSD":"RSD","CZK":"CZK","MNT":"MNT","NZD":"NZD","BGN":"BGN","IDR":"IDR","CNY":"CNY","GEL":"GEL","COP":"COP","ARS":"ARS","PEN":"PEN","CLP":"CLP","POINTS":""};
+window.accountSafeUserName = "Guest";
+window.accountUserThumbnail = "<img class=\"user-profile-image user-default-profile-image\" src=\"\/public\/img\/default_profile_50.png\" width=\"50\" alt=\"\">";
+window.language = "ru";
+window.csrfToken = "T0NkQnRSOS0WMzwIJRRDSj4cUhhMFFBaGgw.e0IAbGsKOSwyID1TQQ==";
+window.requestTime = 1668439375;
+window.requestSimpleSign = "8ee9976884802db8ede2027be23ea3cf";
+window.gcUniqId = "274857749363725d4fc5b3e7.02753767.1668439375.8098";
+window.mailingVisibility = 0;
+window.isSupportChatEnabled = false;
+window.disableCaptchaForConfirmPhone = true;
+window.globalCheckboxEnabled = null;
+window.globalCheckboxText = "\u042f \u0441\u043e\u0433\u043b\u0430\u0441\u0435\u043d \u043d\u0430&nbsp;\u043e\u0431\u0440\u0430\u0431\u043e\u0442\u043a\u0443 \u043c\u043e\u0438\u0445 \u043f\u0435\u0440\u0441\u043e\u043d\u0430\u043b\u044c\u043d\u044b\u0445 \u0434\u0430\u043d\u043d\u044b\u0445 \u0432&nbsp;\u0441\u043e\u043e\u0442\u0432\u0435\u0442\u0441\u0442\u0432\u0438\u0438&nbsp;\u0441&nbsp;<a href=\"\/cms\/system\/legal\" target=\"_blank\">\u0423\u0441\u043b\u043e\u0432\u0438\u044f\u043c\u0438<\/a>";
+window.persodataConfirm = false;
+window.params_52 = 0;
+window.isSessionLocalStorageEnabled = true;
+window.isWhatsappIconEnabled = false;
+window.file_upload_blocking = false;
+window.storage_usage_percent = null;
+window.used_storage_size = null;
+window.max_storage_size = null;
+window.fileserviceThumbnailHost = "fs-thb01.getcourse.ru";
+window.fileserviceMainHost = "fs11.getcourse.ru";
+window.fileserviceUploadHost = "fs08.getcourse.ru";
+window.fileserviceTusFeatureEnabled = true;
+window.isEnabledSecureUpload = 1;
+window.webinarWebsocketHosts = ["ws02.getcourse.ru","ws02.gcfiles.net"];
+window.webSocketHost = "ws04.getcourse.ru";
+window.fdsrevEnabled12 = true;
+window.isDisabledFacebook = true;
+window.isModifiedFacebookButton = false;
+window.metaIsAnExtremismOrganizationText = "\u041a\u043e\u043c\u043f\u0430\u043d\u0438\u044f Meta \u043f\u0440\u0438\u0437\u043d\u0430\u043d\u0430 \u044d\u043a\u0441\u0442\u0440\u0435\u043c\u0438\u0441\u0442\u043a\u043e\u0439 \u0432 \u0420\u0424.";
+window.isDisabledVK = false;
+</script>
+
+<script type="text/javascript">
+
+		
+	window.userInfo = {"isAdmin":false,"isManager":false,"isInAccount":false,"isTeacher":false,"canCms":false,"canEditHelp":false,"isHumanDesign":"0"};
+
+</script>
+
+	<link href="/pl/nassets/a722a989/themes/smoothness/jquery-ui.css?v=1668438052" rel="stylesheet">
+<link href="/pl/nassets/c0c3b40e/css/bootstrap.min.css?v=1668438053" rel="stylesheet">
+<link href="/pl/nassets/f69c05f7/select2.min.css?v=1668438052" rel="stylesheet">
+<link href="/pl/nassets/f69c05f7/select2-kv.min.css?v=1668438052" rel="stylesheet">
+<link href="/pl/nassets/140cade2/css/modal.css?v=1668438053" rel="stylesheet">
+<link href="/pl/nassets/831cf60/css/user-form.css?v=1668438053" rel="stylesheet">
+<link href="/pl/nassets/a603a53/toast/css/jquery.toast.min.css?v=1668438053" rel="stylesheet">
+<link href="/pl/nassets/eac0d13/css/bootstrap-datepicker3.min.css?v=1668438053" rel="stylesheet">
+<link href="/pl/nassets/eac0d13/css/datepicker-kv.min.css?v=1668438053" rel="stylesheet">
+<link href="/pl/nassets/b7b41902/css/global.css?v=1668438053" rel="stylesheet">
+<link href="/pl/nassets/bc97e94a/manrope.css?v=1668438053" rel="stylesheet">
+<link href="/pl/nassets/f2eb2f6d/proxima.css?v=1668438053" rel="stylesheet">
+<link href="/pl/nassets/cbf3e7a2/css/font-awesome.min.css?v=1668438053" rel="stylesheet">
+<link href="/pl/nassets/cbf3e7a2/css/font-viber.min.css?v=1668438053" rel="stylesheet">
+<link href="/pl/nassets/42667aac/css/main.css?v=1668438053" rel="stylesheet">
+<link href="/pl/nassets/42667aac/css/jquery.toast.min.css?v=1668438053" rel="stylesheet">
+<link href="/pl/nassets/42667aac/css/fonts.css?v=1668438053" rel="stylesheet">
+<link href="/pl/nassets/42667aac/css/menu.css?v=1668438053" rel="stylesheet">
+<link href="/pl/nassets/6e4cfeeb/statuses.css?v=1668438053" rel="stylesheet">
+<link href="/pl/nassets/d6a6a737/tasks-block.css?v=1668438053" rel="stylesheet">
+<style></style>
+<script src="/pl/nassets/af3b79e3/jquery.min.js?v=1668438052"></script>
+<script src="/pl/nassets/a722a989/jquery-ui.min.js?v=1668438053"></script>
+<script src="/pl/nassets/adb28243/yii.js?v=1668438053"></script>
+<script src="/pl/nassets/c0c3b40e/js/bootstrap.min.js?v=1668438053"></script>
+<script src="/pl/nassets/42667aac/js/socket.io.slim.js?v=1668438053"></script>
+<script src="/pl/nassets/42667aac/js/socket.js?v=1668438053"></script>
+<script src="/pl/nassets/42667aac/js/websocket.global.js?v=1668438053"></script>
+<script src="/pl/nassets/140cade2/js/modal.js?v=1668438053"></script>
+<script src="/pl/nassets/42667aac/js/react/react.min.js?v=1668438053"></script>
+<script src="/pl/nassets/831cf60/js/user-form-def.js?v=1668438053"></script>
+<script src="/pl/nassets/831cf60/js/build/user-form.js?v=1668438053"></script>
+<script src="/pl/nassets/cdac30f7/jquery.autosize.min.js?v=1668438053"></script>
+<script src="/pl/nassets/a603a53/js/jquery.toast.min.js?v=1668438053"></script>
+<script src="/pl/nassets/cdac30f7/jquery.form.min.js?v=1668438053"></script>
+<script src="/pl/nassets/cdac30f7/jquery.sticky.js?v=1668438053"></script>
+<script src="/pl/nassets/dd0ad559/js/no-conflict.js?v=1668438053"></script>
+<script src="/pl/nassets/eac0d13/js/bootstrap-datepicker.min.js?v=1668438053"></script>
+<script src="/pl/nassets/eac0d13/js/datepicker-kv.min.js?v=1668438053"></script>
+<script src="/pl/nassets/b7b41902/js/main.js?v=1668438053"></script>
+<script src="/pl/nassets/f2b30795/jquery.inputmask.bundle.js?v=1668438053"></script>		</head>
+<body class="gc-user-guest">
+
+
+
+	<div class="gc-main-content gc-both-main-content no-menu  account-page-content gc-user-guest">
+		
+<style>
+	.glyphicon { vertical-align: middle; top: -1px; }
+	.btn { font-size: 15px; padding: 10px 15px; }
+	.gc-main-content.with-left-menu {
+		margin-left: 0;
+	}
+
+	#gcAccountUserMenu {
+		display: none;
+	}
+	.gc-main-content.with-left-menu .container {
+		margin-right: auto;
+		margin-left: auto;
+	}
+</style>
+
+<div class="text-center page-normal-padding" style="text-align: center; padding: 30px 0;">
+
+    <div style="display: block;  ">
+
+        <h3 style="display: inline-block; padding: 30px 40px; border: 8px solid #34495E">Возникла ошибка #400</h3>
+
+        <BR><BR>
+
+        
+
+        <div class="lead">
+            Отсутствуют обязательные параметры: id                    </div>
+
+        <BR>
+
+
+            <input type="hidden" name="message" value="Возникла ошибка 400
+Отсутствуют обязательные параметры: id
+На странице http://cryptomiroslav.getcourse.ru/pl/lite/widget/script">
+
+            <button class="btn btn-info" onclick="location.href='/login'; return false;">
+                <span class="fui fui-arrow-left glyphicon glyphicon-menu-left"></span>Вернуться            </button>
+                            <button class="btn btn-success" onclick="location.href='http://getcourse.ru/contacts/'">
+                    <span class="fui fui-mail glyphicon glyphicon-envelope"></span>
+                    Сообщить в техподдержку GetCourse
+                </button>
+            
+
+    </div>
+
+</div>	</div>
+
+		<script src="/public/js/gccounter.js?1"></script>
+
+
+
+<script src="/pl/nassets/f69c05f7/select2.min.js?v=1668438052"></script>
+<script src="/pl/nassets/f69c05f7/select2-kv.min.js?v=1668438052"></script>
+<script src="/pl/nassets/f69c05f7/select2_locale_ru.js?v=1668438052"></script>
+<script src="/pl/nassets/42667aac/js/light-menu.js?v=1668438053"></script>
+<script src="/pl/nassets/d6a6a737/build/tasks-block.js?v=1668438053"></script></body>
+</html>
